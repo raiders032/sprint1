5989f589e9ed64e297475bc0670063e690397008
raiders032 <nameks@naver.com>
2020-07-14 15:41:04 +0900

    Binding AppProperties & Enabling AppProperties

--- src/main/java/com/swm/sprint1/Sprint1Application.java
+++ src/main/java/com/swm/sprint1/Sprint1Application.java
@@ -1,8 +1,11 @@
 package com.swm.sprint1;
 
+import com.swm.sprint1.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class Sprint1Application {
 
