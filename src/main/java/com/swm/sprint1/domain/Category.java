a6a37a3bee6fb8c0e1db66b199d1a9f3f63ea16d
raiders032 <nameks@naver.com>
2020-07-19 18:40:58 +0900

    domain

---
+++ src/main/java/com/swm/sprint1/domain/Category.java
@@ -0,0 +1,25 @@
+package com.swm.sprint1.domain;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "category", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "name")
+})
+public class Category{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+}
